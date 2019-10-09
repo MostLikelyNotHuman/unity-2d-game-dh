4daeefd630eed675def18178863e0e8c430cefac
MostLikelyNotHuman <55810556+MostLikelyNotHuman@users.noreply.github.com>
2019-10-09 16:44:55 -0500

    'sounds and crate droppage'

---
+++ Gottem/Assets/MyStuff/Scripts/CollectibleManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectibleManager : MonoBehaviour
+{
+    public static CollectibleManager Instance;
+
+    private int _totalGottem;
+    private int _collectedGottem;
+
+    private void Start()
+    {
+        _totalGottem = FindObjectsOfType<Collectible>().Length;
+        DisplayGottemCount();
+    }
+
+    public TextMeshProUGUI gottemCount;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void DisplayGottemCount()
+    {
+        gottemCount.SetText(_collectedGottem + " / " + _totalGottem);
+    }
+
+    public void HandleGottemCollected()
+    {
+        _collectedGottem++;
+        DisplayGottemCount();
+        if(_collectedGottem == _totalGottem)
+        {
+            YouWinGottemMenu.Instance.Show();
+        }
+
+        SoundManager.Instance.PlayCoinCollectSound();
+    }
+}
